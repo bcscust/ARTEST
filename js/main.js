022394fd2ad1eeade02c0dced5430c40c499ee61
bcscust <94435608+bcscust@users.noreply.github.com>
2024-01-16 17:20:17 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ loader.load(
   function (gltf) {
     //If the file is loaded, add it to the scene
     object = gltf.scene;
-    object.scale.set(5000, 5000, 5000);
+    object.scale.set(50, 50, 50);
     scene.add(object);
   },
   function (xhr) {
